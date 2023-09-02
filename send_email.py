6fd543fb9eb04f6fddf68bea73c060412ff4174e
abhi <agrawalabhishek622@gmail.com>
2023-09-02 21:21:16 +0530

    final touch

--- send_email.py
+++ send_email.py
@@ -1,20 +1,16 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "agrawalabhishek622@gmail.com"
-password = "dxnz zxsf zugn bwkb"
+    username = "agrawalabhishek622@gmail.com"
+    password = "dxnz zxsf zugn bwkb"
 
-receiver = "agrawalabhishek622@gmail.com"
-context = ssl.create_default_context()
+    receiver = "agrawalabhishek622@gmail.com"
+    context = ssl.create_default_context()
 
-message = """
-hi!
-how are you?
-bye!
-"""
 
-with smtplib.SMTP_SSL(host, port, context= context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context= context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
