3ff02d1293afc78a49469d8b67aa2ef72a024fca
abhi <agrawalabhishek622@gmail.com>
2023-09-02 18:28:15 +0530

    added image, url and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import  pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -15,5 +16,25 @@ with col2:
     """
     st.info(content)
 
+content2 = """Below you can find some of the app I have built in Python. 
+Feel free to contact me
+"""
+st.write(content2)
 
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("image/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("image/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
